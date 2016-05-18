352eb8fadaa129861d1298036f6e388ebbd06f75
a389523 -- Kadary DEMBELE <kadary.dembele@socgen.com>
2016-05-18 18:26:37 +0200

    Add of module for npm and echart 3 support

--- ngecharts.js
+++ ngecharts.js
@@ -1,50 +1,46 @@
-/**
- * https://github.com/gudh/ngecharts
- * License: MIT
- */
+const angular = require('./node_modules/angular/angular.min.js');
+const echarts = require('./node_modules/echarts/dist/echarts.min');
 
-(function () {
-    'use strict';
+var ngecharts = angular.module('ngecharts', []);
 
-    var app = angular.module('ngecharts', [])
-    app.directive('echarts', ['$window', function ($window) {
-        return {
-            restrict: 'EA',
-            template: '<div></div>',
-            scope: {
-                options: '=options'
-            },
-            link: buildLinkFunc($window)
-        };
-    }]);
+ngecharts.directive('echarts', ['$window', function ($window) {
+    return {
+        restrict: 'EA',
+        template: '<div></div>',
+        scope: {
+            options: '=options'
+        },
+        link: buildLinkFunc($window)
+    };
+}]);
 
-    function buildLinkFunc($window) {
-        return function (scope, ele, attrs) {
-            var chart, options;
-            chart = echarts.init(ele[0], 'macarons'); 
+function buildLinkFunc($window) {
+    return function (scope, ele, attrs) {
+        var chart, options;
+        chart = echarts.init(ele[0], 'macarons');
 
-            createChart(scope.options);
+        createChart(scope.options);
 
-            function createChart(options) {
-                if (!options) return;
+        function createChart(options) {
+            if (!options) return;
 
-                chart.setOption(options); 
-                // scope.$emit('create', chart);
+            chart.setOption(options);
+            // scope.$emit('create', chart);
 
-                angular.element($window).bind('resize', function(){
-                    chart.resize();
-                });
+            angular.element($window).bind('resize', function(){
+                chart.resize();
+            });
 
-            }
+        }
 
-            scope.$watch('options', function (newVal, oldVal) {
-                if (angular.equals(newVal, oldVal)) return;
-                createChart(newVal);
-            })
+        scope.$watch('options', function (newVal, oldVal) {
+            if (angular.equals(newVal, oldVal)) return;
+            createChart(newVal);
+        })
 
-            
-        };
-    }
 
-})(); 
+    };
+}
+
+module.exports = ngecharts;
 
